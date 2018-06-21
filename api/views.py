44327f0054569fa91a9fb618c535e0b9d1b80fb9
kalyango <johnkal24@gmail.com>
2018-06-21 23:09:57 +0300

    Update all-rides

--- api/views.py
+++ api/views.py
@@ -1,15 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Module defines views """
 from flask import jsonify, request
 from flask.views import MethodView
+class GetRide(MethodView):
+    """
+    class that defines views
+    """
+    rides = [
+        {'id':1, 'time_to_leave' : '12:30 pm', 'price':'5000/=', 'start':'makerere', 'destination':'Bukoto', 'Driver_name':'Mutano Jude'},
+        {'id':2, 'time_to_leave' : '12:30 pm', 'price':'400/=', 'start':'Kikoni', 'destination':'Munyonyo', 'Driver_name':'Mutano jude'},
+        {'id':3, 'time_to_leave' : '12:30 pm', 'price':'25000/=', 'start':'Ntinda', 'destination':'Makerere', 'Driver_name':'Faith Shella'},
+        {'id':4, 'time_to_leave' : '12:30 pm', 'price':'500/=', 'start':'makerere', 'destination':'Ntida', 'Driver_name':'Faith Shella'},
+        ]
 
-class Get_ride(MethodView):
-    rides =[{'name' : 'kalyango','start':'muk','destination':'ntida'},{'name' : 'kalyango2','start':'muk','destination':'ntida'}, {'name' : 'kalyango3','start':'muk','destination':'ntida'}, {'name' : 'kalyango4','start':'muk','destination':'ntida'}]
-    def get(self, name):
-        if name == None:
+    def get(self, ride_id):
+        """
+        method for all get requests
+        """
+        if ride_id == None:
             return jsonify({'rides':self.rides})
-        rids = [ride for ride in self.rides if ride['name'] == name]
+        rids = [ride for ride in self.rides if ride['id'] == ride_id]
         return jsonify({'ride' : rids[0]})
-    def post(self):
-        ride = {'name' : request.json['name']}
-        self.rides.append(ride)
-        return jsonify({'rides' : self.rides})
+        
+  
         
