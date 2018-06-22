38b5398053bdd2d3d9fce80dc40be649335cdb37
kalyango <johnkal24@gmail.com>
2018-06-22 16:39:43 +0300

    Add travis file and coverage

--- api/test_views.py
+++ api/test_views.py
@@ -2,6 +2,8 @@
 """This is the main module"""
 import unittest
 import json
+from json import JSONEncoder
+
 from app import APP
 class TestViews(unittest.TestCase):
     """
@@ -42,6 +44,7 @@ class TestViews(unittest.TestCase):
         testing for request to join a ride
         """
         result = self.client().post('/api/v1/rides/2/requests', content_type="application/json", data=json.dumps(dict(id=4, passenger_name="Junior Sara", phone_number="078966857")))
-        self.assertEqual(result.status_code, 200)
-        self.assertTrue(result.json["requests"])
+        
+        self.assertTrue(result.json["reqs"])
         self.assertIn(b"ride_id", result.data)
+        
